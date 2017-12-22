c9907dec352a5409446a5c97ef2a5f1607af0ec1
Abdul Shabbir <abdulqshabbir@gmail.com>
2017-12-22 22:21:09 +0300

    keys linked to sounds using data keys. Event listeners triggered on keydown and transitions reversed using the event-listener property 'transitionend'. Background image added and layout cleaned up.

--- script.js
+++ script.js
@@ -1,51 +1,20 @@
-//--- retrieve all sounds from html and convert to javascript objects
-const eString = new Audio("guitarSounds/1st_String_E_64kb.mp3");
-const bString = new Audio("guitarSounds/2nd_String_B__64kb.mp3");
-const gString = new Audio("guitarSounds/3rd_String_G_64kb.mp3");
-const dString = new Audio("guitarSounds/4th_String_D_64kb.mp3");
-const aString = new Audio("guitarSounds/5th_String_A_64kb.mp3");
-const eSTring = new Audio ("guitarSounds/6th_String_E_64kb.mp3");
-const cChord = new Audio ("guitarSounds/C_64kb.mp3");
-const dChord = new Audio ("guitarSounds/C_64kb.mp3");
-const dmChord = new Audio("guitarSounds/D_64kb.mp3");
-const eChord = new Audio ("guitarSounds/Dm_64kb.mp3");
 
-//--change the playback to be 4 times faster --
-eString.playbackRate = 4;
-bString.playbackRate = 4;
-gString.playbackRate = 4;
-aString.playbackRate = 4;
-eString.playbackRate = 4;
-eSTring.playbackRate = 4;
-cChord.playbackRate = 4;
-dChord.playbackRate = 4;
-dmChord.playbackRate = 4;
-eChord.playbackRate = 4;
+window.addEventListener('keydown', function(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return;//stop running if keypress does not return audio file
+  audio.currentTime = 0;//play audio from start after each press
+  audio.play();
 
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);//get key pressed
+  console.log(key.classList);
+  key.classList.add('playing')
+});
 
-//retrieve all home key codes for click events
-const A_HOME_KEY = 65;
-const S_HOME_KEY = 83;
-const D_HOME_KEY = 68;
-const F_HOME_KEY = 70;
-const G_HOME_KEY = 71;
-const H_HOME_KEY = 72;
-const J_HOME_KEY = 74;
-const K_HOME_KEY = 91;
-const L_HOME_KEY = 76;
-const SEMICOLON_HOME_KEY = 186;
+const keys = Array.from(document.querySelectorAll('.key'));
 
-//retrieve all
-
-const aKey = document.getElementById('a');
-
-function animateLetter(e) {
-    aKey.style.cssText = "border: 2px solid yellow;";
+function removeTransition(e) {
+  this.classList.remove('playing'); 
 }
 
-document.addEventListener("keydown", (e) => {
-  if(e.keyCode === A_HOME_KEY) {
-    eString.play();
-    animateLetter(e);
-  }
-});
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
